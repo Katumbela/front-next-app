bc927848e8429a188dc1b0fec9586a71d4ccffd0
Katumbela <ja3328173@gmail.com>
2024-08-24 21:38:29 +0100

    feat: test/src/utils/image.exporter.ts. Criação de novo arquivo

---
+++ test/src/utils/image.exporter.ts
@@ -0,0 +1,14 @@
+import icon1 from "@/assets/icons/Dakirby309-Simply-Styled-Mac-Launchpad.256.png"
+import icon2 from "@/assets/icons/Dakirby309-Simply-Styled-Mac-iCal.256.png"
+import icon3 from "@/assets/icons/Hamzasaleem-Stock-Folder-Folder-Server.512.png"
+import icon4 from "@/assets/icons/Tatice-Cristal-Intense-Apple-multicolor.256.png"
+import icon5 from "@/assets/icons/icons8-mac-logo.svg"
+
+
+export const icons = {
+  icon1,
+  icon3,
+  icon2,
+  icon4,
+  icon5
+} as const
